108b3989606657efc94b25deabcd0b29be0096d4
Chantip-P <act21974@gmail.com>
2024-12-04 16:27:52 +0700

    Change main.jsx to not use react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'  // Change this line
 import './index.css'
 import Homepage from './components/Homepage.jsx'
 import Aboutpage from './components/AboutPage.jsx'
 import TabbedMenu from './components/TabbedMenu.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element:<Homepage />
-  },
-  {
-    path: "about",
-    element:<Aboutpage />
-  },
-  {
-    path: "menu",
-    element:<TabbedMenu />
-  },
-
-]);
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="about" element={<Aboutpage />} />
+        <Route path="menu" element={<TabbedMenu />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 )
